6e822a888cd1f0d6fd5a57739d79d64a84e816f9
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-25 21:42:22 +1300

    finished the functionaality
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/component/Card.js
+++ src/component/Card.js
@@ -10,26 +10,27 @@ const Card = () => {
 	const fetchAdvice = async () => {
 		try {
 			const { data } = await axios(url);
-      const{id,advice} = (data.slip);
-      setId(id);
-      setAdvice(advice);
+			const { id, advice } = data.slip;
+			setId(id);
+			setAdvice(advice);
 		} catch (error) {
 			console.log(error.response);
 		}
 	};
 	fetchAdvice();
 
-	return <section>
-    <div className="card">
-      <h1 className ='card-header'>Advice # {id}</h1>
-      <p className="card-body">{advice}</p>
-      <div className="separator"></div>
-      <button className="btn" onClick={fetchAdvice}>random button</button>
-    </div>
-
-
-
-  </section>;
+	return (
+		<section>
+			<div className='card'>
+				<h1 className='card-header'>Advice # {id}</h1>
+				<p className='card-body'>{advice}</p>
+				<div className='separator'></div>
+				<button className='btn' onClick={fetchAdvice}>
+					random button
+				</button>
+			</div>
+		</section>
+	);
 };
 
 export default Card;
