59aa47732259529bed848404f2d9fb01aacb8b4b
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-26 01:46:15 +1300

    added a footer.js commponent

---
+++ src/component/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='footer'>
+			<div className='attribution'>
+				<p>
+					Challenge by:
+					<a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
+						Frontend Mentor
+					</a>
+					. Coded by{' '}
+					<a href='https://hcmwebs.com/' target='_blank'>
+						Hcmwebs
+					</a>
+					.
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
