e488773b4130ff00cc6f726c1e90937f851972c3
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-27 03:12:57 +1300

    added  medias to the main
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/component/Attributions.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Attributions = () => {
+	return (
+		<div className='attribution'>
+			<small className='small'>
+				Challenge bygit 
+				<a
+					href='https://www.frontendmentor.io?ref=challenge'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Frontend Mentor
+				</a>
+				Coded by
+				<a
+					href='https://hcmwebs.com/'
+					target='_blank'
+					rel='noopener noreferrer'>
+					Hcmwebs
+				</a>
+			</small>
+		</div>
+	);
+};
+
+export default Attributions;
