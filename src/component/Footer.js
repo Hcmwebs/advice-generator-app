71fe33bad523d08d8f816aae972926c638e9570b
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-03-27 03:23:39 +1300

    created the attribution js component  for the Footer
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,21 +1,10 @@
 import React from 'react';
+import Attributions from './Attributions';
 
 const Footer = () => {
 	return (
 		<footer className='footer'>
-			<div className='attribution'>
-				<p>
-					Challenge by:
-					<a href='https://www.frontendmentor.io?ref=challenge' target='_blank'>
-						Frontend Mentor
-					</a>
-					. Coded by{' '}
-					<a href='https://hcmwebs.com/' target='_blank'>
-						Hcmwebs
-					</a>
-					.
-				</p>
-			</div>
+			<Attributions />
 		</footer>
 	);
 };
