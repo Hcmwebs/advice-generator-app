94abfb1b5fb13d0da621a70aec8d22c2cf046c09
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-05-02 16:51:22 +1200

    corrected a typo
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/component/Card.js
+++ src/component/Card.js
@@ -30,38 +30,36 @@ const Card = () => {
 	}, [])
 
 	return (
-
-				<main>
-					<div className='card'>
-						<h1 className='card-header'>Advice #{id}</h1>
-						<q className='card-body'>{advice}</q>
-						<picture>
-							<source srcSet={patternDesktop} media='(max-width: 1440px)' />
-							<source srcSet={patternMobile} media='(max-width: 375px)' />
-							<img
-								src={patternMobile}
-								alt='pattern divider mobile'
-								className='img'
-							/>
-						</picture>
-						<button
-							className={`${isLoading ? 'btn active' : 'btn'}`}
-							onClick={fetchAdvice}
-							aria-label='fetch advice'>
-							<svg
-								className='icon-dice'
-								width='24'
-								height='24'
-								xmlns='http://www.w3.org/2000/svg'>
-								<path
-									d='M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z'
-									fill='#202733'
-								/>
-							</svg>
-						</button>
-					</div>
-				</main>
-
+		<main>
+			<div className='card'>
+				<h1 className='card-header'>Advice #{id}</h1>
+				<q className='card-body'>{advice}</q>
+				<picture>
+					<source srcSet={patternDesktop} media='(max-width: 1440px)' />
+					<source srcSet={patternMobile} media='(max-width: 375px)' />
+					<img
+						src={patternMobile}
+						alt='pattern divider mobile'
+						className='img'
+					/>
+				</picture>
+				<button
+					className={`${isLoading ? 'btn active' : 'btn'}`}
+					onClick={fetchAdvice}
+					aria-label='fetch advice'>
+					<svg
+						className='icon-dice'
+						width='24'
+						height='24'
+						xmlns='http://www.w3.org/2000/svg'>
+						<path
+							d='M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z'
+							fill='#202733'
+						/>
+					</svg>
+				</button>
+			</div>
+		</main>
 	)
 }
 
