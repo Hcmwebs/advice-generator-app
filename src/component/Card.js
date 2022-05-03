ae5a0bd85d92cf65e67ca7b3665a364f8f25bf41
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-05-03 12:02:44 +1200

    adjusted the speed loading animation
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/component/Card.js
+++ src/component/Card.js
@@ -6,7 +6,7 @@ import patternDesktop from '../images/pattern-divider-desktop.svg'
 const url = 'https://api.adviceslip.com/advice'
 
 const Card = () => {
-	const [isLoading, setIsLoading] = useState(true)
+	const [isLoading, setIsLoading] = useState(false)
 	const [id, setId] = useState(1)
 	const [advice, setAdvice] = useState('Advice goes here')
 
@@ -29,6 +29,21 @@ const Card = () => {
 		fetchAdvice()
 	}, [])
 
+
+	if(isLoading){
+		return <button className="btn active">
+			<svg
+						className='icon-dice icon-dice-lg'
+						width='24'
+						height='24'
+						xmlns='http://www.w3.org/2000/svg'>
+						<path
+							d='M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z'
+							fill='#202733'
+						/>
+					</svg>
+		</button>
+	}
 	return (
 		<main>
 			<div className='card'>
